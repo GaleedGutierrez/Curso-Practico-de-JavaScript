129493669281044ef6cacc07a1b47780c9259efe
Galeed Gutierrez <galeedeveloper@gmail.com>
2021-12-12 21:34:08 -0300

    Clase N°8: Interactuando con JavaScript desde HTML.

--- figuras.js
+++ figuras.js
@@ -1,45 +1,42 @@
-// Código del cuadrado.
-// const squareSide = 5;
+// Functions squeare
+function calculateSquarePerimeter() {
+    const input = document.getElementById("InputSquare");
+    const value = input.value;
+    const perimeter = squarePerimeter(value);
+    alert(perimeter);
+}
+
+function calculateSquareArea() {
+    const input = document.getElementById("InputSquare");
+    const value = input.value;
+    const area = squareArea(value);
+    alert(area);
+}
 
 const squarePerimeter = (side) => side * 4;
-
-// squarePerimeter();
-
 const squareArea = (side) => side ** 2;
 
-console.group("Square");
-// console.log(`The sides of the square measure: ${squareSide}u`);
-// console.log(`The perimeter of the square is: ${squarePerimeter}u`);
-// console.log(`The area of the square is: ${squareArea}u^2`);
-console.groupEnd();
-
-// Código del triangulo.
-// const triangleSide1 = 6;
-// const triangleSide2 = 6;
-// const triangleBase = 4;
-// const triangleHeight = 5.5;
+// Functions triangle
 const trianglePerimeter = (side1, side2, base) => side1 + side2 + base;
-
 const triangleArea = (base, height) => base * height / 2;
 
-console.group("Triangle");
-// console.log(`The sides of the triangle measure: ${triangleSide1}u, ${triangleSide2}u and ${triangleBase}u.`);
-// console.log(`The height of the triangle is: ${triangleHeight}u`);
-// console.log(`The perimeter of the triangle is: ${trianglePerimeter}u`);
-// console.log(`The area of the triangle is: ${triangleArea}u^2`);
-console.groupEnd();
-
-// Código del circulo.
-// const circleRadio = 4;
-const pi = Math.PI;
+// Functions circle
 const circleDiameter = (radio) => 2 * radio;
 const circumference = (radio) => pi * circleDiameter(radio);
 const circleArea = (radio) => pi * radio ** 2;
 
+// Code square
+console.group("Square");
+
+console.groupEnd();
+
+// Código del triangulo.
+console.group("Triangle");
+
+console.groupEnd();
+
+// Código del circulo.
+const pi = Math.PI;
 console.group("Circle");
-// console.log(`The radio of the circle is: ${circleRadio}u.`);
-// console.log(`The diameter of the circle is: ${circleDiameter}u.`);
-// console.log(`The circunmference of the circle is: ${circumference}u.`);
-// console.log(`The area of the circle is: ${circleArea}u^2.`);
-// console.log(`Pi is: ${pi}.`);
+
 console.groupEnd();
