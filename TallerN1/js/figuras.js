cb6d2f399eafff97b4fed17f09ffb5203551b119
Galeed Gutierrez <galeedeveloper@gmail.com>
2021-12-14 05:24:12 -0300

    Taller N°1: Sección insertar medidas, las cajas aparecen falta poder agregar datos y poder operar con ellos..

--- TallerN1/js/figuras.js
+++ TallerN1/js/figuras.js
@@ -18,7 +18,7 @@ const squareArea = (side) => side ** 2;
 
 // Functions triangle
 const trianglePerimeter = (side1, side2, base) => side1 + side2 + base;
-const triangleArea = (base, height) => base * height / 2;
+const triangleArea = (base, height) => (base * height) / 2;
 
 // Functions circle
 const circleDiameter = (radio) => 2 * radio;
@@ -26,20 +26,20 @@ const circumference = (radio) => pi * circleDiameter(radio);
 const circleArea = (radio) => pi * radio ** 2;
 
 // Code square
-console.group("Square");
+// console.group("Square");
 
-console.groupEnd();
+// console.groupEnd();
 
 // Código del triangulo.
-console.group("Triangle");
+// console.group("Triangle");
 
-console.groupEnd();
+// console.groupEnd();
 
 // Código del circulo.
 const pi = Math.PI;
-console.group("Circle");
+// console.group("Circle");
 
-console.groupEnd();
+// console.groupEnd();
 
 // function changeImg() {
 //     const inputCheckbox = document.getElementById("selectSquareBox");
@@ -52,5 +52,95 @@ console.groupEnd();
 //     }
 // }
 
-const imgUrlGoogleDrive = "https://drive.google.com/uc?id="
+const imgUrlGoogleDrive = "https://drive.google.com/uc?id=";
 
+function addingDimensionBoxes() {
+    let selectedFigure;
+    const boxSquare = document.getElementById("measurement-square");
+    const boxTriangle = document.getElementById("measurement-triangle");
+    const boxCircle = document.getElementById("measurement-circle");
+    const boxRectangle = document.getElementById("measurement-rectangle");
+    const boxParallelogram = document.getElementById(
+        "measurement-parallelogram"
+    );
+    const boxDiamond = document.getElementById("measurement-diamond");
+    const boxTrapeze = document.getElementById("measurement-trapeze");
+    const boxPolygon = document.getElementById("measurement-polygon");
+
+    const boxesFigures = [
+        boxSquare,
+        boxTriangle,
+        boxCircle,
+        boxRectangle,
+        boxParallelogram,
+        boxDiamond,
+        boxTrapeze,
+        boxPolygon,
+    ];
+
+    for (box of boxesFigures) {
+        box.classList.remove("visible");
+        box.classList.add("hidden");
+    }
+
+    const squareSelected = document.getElementById("squareFigure");
+    const triangleSelected = document.getElementById("triangleFigure");
+    const circleSelected = document.getElementById("circleFigure");
+    const rectangleSelected = document.getElementById("rectangleFigure");
+    const parallelogramSelected = document.getElementById(
+        "parallelogramFigure"
+    );
+    const diamondSelected = document.getElementById("diamondFigure");
+    const trapezeSelected = document.getElementById("trapezeFigure");
+    const polygonSelected = document.getElementById("polygonFigure");
+
+    const figures = [
+        squareSelected,
+        triangleSelected,
+        circleSelected,
+        rectangleSelected,
+        parallelogramSelected,
+        diamondSelected,
+        trapezeSelected,
+        polygonSelected,
+    ];
+
+    for (let figure of figures)
+        if (figure.checked === true) 
+            selectedFigure = figure;
+
+    switch (selectedFigure) {
+        case squareSelected:
+            boxSquare.classList.remove("hidden");
+            boxSquare.classList.add("visible");
+            break;
+        case triangleSelected:
+            boxTriangle.classList.remove("hidden");
+            boxTriangle.classList.add("visible");
+            break;
+        case circleSelected:
+            boxCircle.classList.remove("hidden");
+            boxCircle.classList.add("visible");
+            break;
+        case rectangleSelected:
+            boxRectangle.classList.remove("hidden");
+            boxRectangle.classList.add("visible");
+            break;
+        case parallelogramSelected:
+            boxParallelogram.classList.remove("hidden");
+            boxParallelogram.classList.add("visible");
+            break;
+        case diamondSelected:
+            boxDiamond.classList.remove("hidden");
+            boxDiamond.classList.add("visible");
+            break;
+        case trapezeSelected:
+            boxTrapeze.classList.remove("hidden");
+            boxTrapeze.classList.add("visible");
+            break;
+        case polygonSelected:
+            boxPolygon.classList.remove("hidden");
+            boxPolygon.classList.add("visible");
+            break;
+    }
+}
