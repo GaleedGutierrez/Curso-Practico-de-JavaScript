d199e7a5ef0c2faddaca5cfa74c0108810d26d20
Galeed Gutierrez <galeedeveloper@gmail.com>
2021-12-22 16:58:09 -0300

    Calculando la mediana en Javascript y reto terminado.

---
+++ TallerN3/mediana.js
@@ -0,0 +1,56 @@
+function calculateArithmeticMean(list) {
+    const sumList = list.reduce((previusValue, currentValue) => previusValue + currentValue);
+    const averageList = sumList / list.length;
+
+    return averageList;
+}
+
+function isEven(number) {
+    let even = false;
+    if (number % 2 === 0)
+        even = true;
+    return even;
+}
+
+function calculateMedian(list) {
+    let median = 0;
+    const halfList = Math.floor(list.length / 2);
+
+    if (isEven(list1.length)) {
+        const element1 = list[halfList];
+        const element2 = list[halfList - 1];
+        const elements = [element1, element2];
+        median = calculateArithmeticMean(elements);
+    } 
+    else
+        median = list1[halfList];
+
+    return median;
+}
+
+function organizeIncresingOrder(list) {
+    let listOrganize = list.sort((a, b) => a - b);
+    return listOrganize;
+}
+
+let list1 = [
+    10,
+    8,
+    5,
+    15,
+    23,
+    65,
+    98,
+    33,
+    3,
+    2
+]
+
+list1 = organizeIncresingOrder(list1);
+
+const median = calculateMedian(list1);
+
+
+console.log({
+    list1, median
+});
